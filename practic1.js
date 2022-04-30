12d0728eea50848ca32cda138410ebe050a0cca6
kategrom <95121842+kategrom@users.noreply.github.com>
2022-04-30 14:31:41 +0300

    Add files via upload

---
+++ practic1.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// task 1
+
+const str = "Привіт";
+console.log(typeof str);
+
+const num = 123;
+console.log(typeof num);
+
+const flag = true;
+console.log(typeof flag);
+
+const txt = "true";
+console.log(typeof txt);
+
+// task 2
+
+const a1 = "Слава Україні!";
+let b1 = "Героям Слава!";
+
+const a2 = 2022;
+let b2 = 2023;
+
+const a3 = true;
+let b3 = false;
+
+const a4 = null;
+let b4 = null;
+
+const a5 = undefined;
+let b5 = {};
